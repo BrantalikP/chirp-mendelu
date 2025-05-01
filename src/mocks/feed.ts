e2cced32f68eb1bce8a0a4c7bb9f74c46ce246c9
brantalikp <brantalikp@gmail.com>
2025-05-01 19:07:52 +0200

    expo 53

--- src/mocks/feed.ts
+++ src/mocks/feed.ts
@@ -8,7 +8,7 @@ export const posts = [
         "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/45.png",
     },
     content: "BELIEVE IT! 🦊🔥 Just finished my bowl of Ichiraku ramen 🍜",
-    createdAt: "2025-03-24T14:12:00Z",
+    createdAt: "2025-05-01T14:12:00Z",
     image:
       "https://a.storyblok.com/f/178900/638x358/399291d251/8ad71330b958165e3b735054f4ff20211498090822_full.jpg/m/637x357",
     stats: {
@@ -26,7 +26,7 @@ export const posts = [
         "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/20.png",
     },
     content: "Meat is life. 🥩☠️ #StrawHatCrew",
-    createdAt: "2025-03-24T13:30:00Z",
+    createdAt: "2025-05-01T13:30:00Z",
     stats: {
       comments: 9,
       retweets: 18,
@@ -42,7 +42,7 @@ export const posts = [
         "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/2.png",
     },
     content: "I’m the strongest. 😎🌀 #SixEyes",
-    createdAt: "2025-03-23T21:45:00Z",
+    createdAt: "2025-04-29T21:45:00Z",
     stats: {
       comments: 23,
       retweets: 55,
@@ -58,7 +58,7 @@ export const posts = [
         "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/7.png",
     },
     content: "Yare yare daze... Just ORA’d my way through the day. 🧢🕶️",
-    createdAt: "2025-03-22T10:05:00Z",
+    createdAt: "2025-04-29T10:05:00Z",
     image:
       "https://preview.redd.it/how-did-jotaro-get-the-items-in-the-jail-cell-at-the-v0-nk9rqc60drgb1.jpg?width=640&crop=smart&auto=webp&s=70ee5d36d541697487a5a1f1b48c4dac32d8b11b",
     stats: {
@@ -76,7 +76,7 @@ export const posts = [
         "https://raw.githubusercontent.com/Ashwinvalento/cartoon-avatar/master/lib/images/male/17.png",
     },
     content: "This place is filthy. Let me clean up. 🧼⚔️",
-    createdAt: "2025-03-21T16:25:00Z",
+    createdAt: "2025-04-29T16:25:00Z",
     stats: {
       comments: 4,
       retweets: 7,
